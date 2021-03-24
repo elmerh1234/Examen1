a032bf91df17f419babb1695492f69a487cfd3cf
ivan_hernandez <ivan_hernandez@ISSVGALL7CQV>
2021-03-24 15:07:28 -0600

    Agregar archivos de proyecto.

---
+++ Examen1/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Examen1.Models;
+
+namespace Examen1.Controllers
+{
+    public class HomeController : Controller
+    {
+        DAOUsuarios dao = new DAOUsuarios();
+        
+        public ActionResult Index()
+        {
+            if(Session["nivel"]==null)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return View();
+            }
+   
+        }
+
+        public ActionResult About()
+        {         
+
+            if (Session["nivel"] == null)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                ViewBag.Message = "Your application description page.";
+                return View();
+            }
+        }
+
+        public ActionResult Contact()
+        {
+            if (Session["nivel"] == null)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                ViewBag.Message = "Your contact page.";
+                return View();
+            }
+        }
+
+        public ActionResult Helpers()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string txtUsu, string txtContra)
+        {
+            string usu = txtUsu;
+            string contra = txtContra;
+            string nivel = dao.login(usu, contra);
+
+            if (!nivel.Equals(""))
+            {
+                System.Web.HttpContext.Current.Session["nivel"] = nivel;
+                System.Web.HttpContext.Current.Session["nombre"] = usu;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ Examen1/Models/DAOUsuarios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Examen1.Models
+{
+    public class DAOUsuarios
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+
+        public DAOUsuarios()
+        {
+            this.con.ConnectionString = @"Data Source=192.00.00.00; Initial Catalog=examen1ds39; User ID=sa;Password=Passw0rd;";
+
+        }
+
+        public string login(string usu, string contra)
+        {
+            string nivel = "";
+            this.cmd.CommandText = "select nivel_usuario from usuarios where nombre_usuario='" + usu + "' and contra='" + contra + "'";
+            this.cmd.Connection = con;
+            this.cmd.Connection.Open();
+            SqlDataReader lector = this.cmd.ExecuteReader();
+            while(lector.Read())
+            {
+                nivel = lector[0].ToString();
+            }
+
+            return nivel;
+        }
+    }
+}
